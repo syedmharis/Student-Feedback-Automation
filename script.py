bee8bbbb8f029da08ce863a11df62312cd518850
harrylovescoding <syedharry100@gmail.com>
2023-05-11 19:14:33 +0500

    Final Version

--- script.py
+++ script.py
@@ -1,11 +1,10 @@
 import asyncio
 from playwright.async_api import async_playwright, ElementHandle
 
-async def submit_survey(RollNo, Password):
+async def submit_survey(RollNo, Password,user_rating):
     async with async_playwright() as p:
-        forms = 14
-       
-
+        forms = '08'
+        
         browser = await p.chromium.launch(headless=False)
         page = await browser.new_page()
 
@@ -16,23 +15,32 @@ async def submit_survey(RollNo, Password):
         await page.fill('#txtRegistrationNo_cs', RollNo)
         await page.fill('#txtPassword_m6cs', Password)
 
+        # Wait 2 seconds
+        await asyncio.sleep(2)
+
         # Submit the login form
         await page.click('#btnlgn')
 
         # Wait for the survey page to load
         await page.wait_for_selector('#kt_content')
+        await asyncio.sleep(2)
 
         # Navigate to the survey Forms
         await page.goto('http://erp.uit.edu:803/studentportalbeta/survey/2')
 
-        for i in range(5):
+        for i in range(10):
 
             # Wait for the form button to appear and click it
             await page.wait_for_selector('#ctl00_ContentPlaceHolder1_TgridSurvey')
             await page.click(f'#ctl00_ContentPlaceHolder1_TgridSurvey_ctl00_ctl{forms}_RadButton1')
 
             # Select the rating for each radio button
-            radio_count = 1052
+            while True:
+                if user_rating.isdigit() and 1052 <= int(user_rating) <= 1054:
+                    break
+                else:
+                    print("Invalid input. Please enter a number between 1052 and 1054.")
+            radio_count = int(user_rating)
             for i in range(280, 296):
                 radio_button = await page.wait_for_selector(f'#rdo{radio_count}')
                 await radio_button.click()
@@ -52,8 +60,9 @@ async def submit_survey(RollNo, Password):
             await asyncio.sleep(1) # Add a delay of 1 second
 
             # Submit the form
-            submit_button = await page.wait_for_selector('#btnSubmit')
-            await submit_button.click()
+            await page.wait_for_selector('#btnSubmit')
+            await page.click('#btnSubmit', delay=1000)  # wait 1 second between mouse down and up events
+
 
             # Wait for the alert to appear and accept it
             alert = await page.wait_for_event('dialog')
@@ -63,18 +72,18 @@ async def submit_survey(RollNo, Password):
 
             # Back to Portal
             Portal_button = await page.wait_for_selector('#btnbackToPortal')
+            await asyncio.sleep(2)
             await Portal_button.click()
 
             # Wait for the success message
             await page.wait_for_selector('#ctl00_ContentPlaceHolder1_TgridSurvey')
 
-            forms = forms+2
-
-            
+            forms = int(forms)+2
 
         # Close the browser
         await browser.close()
 
 my_rollno = input("Enter your Roll No: ")
 my_password = input("Enter your Password: ")
-asyncio.run(submit_survey(my_rollno,my_password))
+user_rating = input("Enter your rating (1052 For 5, 1053 For 4, 1052 For 3): ")
+asyncio.run(submit_survey(my_rollno,my_password,user_rating))
